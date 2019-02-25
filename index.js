8dcf9ce70c1e541d994bae2edc4ad80a577316eb
bhumip214 <bhumip214@gmail.com>
2019-02-25 14:49:14 -0800

    GET users with restricted middleware, POST register, and POST login done.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require("morgan");
+const bcrypt = require("bcryptjs");
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -12,6 +16,88 @@ server.use(morgan("dev"));
 
 server.get("/", (req, res) => {
   res.send(`<h2> Lambda - Authentication Project </h2>`);
+});
+
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    db("users")
+      .where({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Ran into an unexprected errors" });
+      });
+  } else {
+    res.status(500).json({ message: "NO credentials provided" });
+  }
+}
+
+server.get("/api/users", restricted, async (req, res) => {
+  try {
+    const users = await db("users");
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(401).json({
+      message: "You shall not pass!"
+    });
+  }
+});
+
+server.post("/api/register", async (req, res) => {
+  try {
+    if (req.body.username && req.body.password) {
+      const hash = await bcrypt.hashSync(req.body.password, 14);
+      req.body.password = hash;
+
+      const [id] = await db("users").insert(req.body);
+      const user = await db("users")
+        .where({ id })
+        .first();
+      res.status(201).json(user);
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide username and password to register."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error while registering."
+    });
+  }
+});
+
+server.post("/api/login", async (req, res) => {
+  try {
+    const { username, password } = await req.body;
+    if (username && password) {
+      const user = await db("users")
+        .where({ username })
+        .first();
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    } else {
+      res.status(400).json({
+        errorMessage:
+          "Please provide correct username and password to register."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error while logging in."
+    });
+  }
 });
 
 const port = process.env.PORT || 5000;
